1877c0270373004781f590305a696fd25442b9ad
msafryx <mzafry2002@gmail.com>
2025-06-28 11:06:32 +0530

    added the child screen and settings screen

--- components/ui/childcare/parent/ParentHomeStack.tsx
+++ components/ui/childcare/parent/ParentHomeStack.tsx
@@ -2,8 +2,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import BookSitterScreen from "./BookSitterScreen";
 import ChatbotScreen from "./ChatbotScreen";
+import ChildProfilesScreen from "./ChildProfilesScreen";
 import ParentHomeScreen from "./ParentHomeScreen";
 import ScheduleScreen from "./ScheduleScreen";
+import SettingsScreen from "./SettingsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,8 @@ export default function ParentHomeStack() {
       <Stack.Screen name="BookSitter" component={BookSitterScreen} />
       <Stack.Screen name="Chatbot" component={ChatbotScreen} />
       <Stack.Screen name="Schedule" component={ScheduleScreen} />
+      <Stack.Screen name="ChildProfiles" component={ChildProfilesScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
 }
